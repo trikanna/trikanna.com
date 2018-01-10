73a929d6e63e84ec10d553955867bccb9f300caa
Santiago Camelo <trikanna@gmail.com>
2018-01-10 17:00:47 -0300

    empty

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,48 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Gatsby Default Starter'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-61808521-1',
+        anonymize: false
+      }
+    },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: './src/favicon.png',
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: true,
+          favicons: true,
+          firefox: true,
+          twitter: true,
+          yandex: true,
+          windows: true
+        }
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Santiago Camelo',
+        short_name: 'trikanna',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#007aff',
+        display: 'minimal-ui'
+      }
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`
+  ]
 };
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,35 +1,51 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 
-import Header from '../components/Header'
-import './index.css'
+import Header from '../components/Header';
+
+import './reset.scss';
+import './global.scss';
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Santiago Camelo"
+      htmlAttributes={{ lang: 'es' }}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        {
+          name: 'description',
+          content: 'Santiago Camelo, one commit at a time'
+        },
+        {
+          name: 'keywords',
+          content: 'blog, react, react native, blog, vlog, cursos, tutoriales, gratis'
+        },
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1.0'
+        },
+        {
+          name: 'apple-mobile-web-app-capable',
+          content: 'yes'
+        },
+        {
+          name: 'apple-mobile-web-app-status-bar-style',
+          content: 'default'
+        },
+        {
+          name: 'apple-mobile-web-app-title',
+          content: 'Santiago Camelo'
+        }
       ]}
     />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+
+    <div>{children()}</div>
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
+  children: PropTypes.func
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
